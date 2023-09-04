10430b5b6cf890fa88838fe918a04ccc9eb5533a
Malderna <142558736+Malderna@users.noreply.github.com>
2023-09-03 21:09:43 -0400

    Add files via upload

---
+++ SUBASTAS/Persona.java
@@ -0,0 +1,34 @@
+// WARNING: This file is auto-generated and any changes to it will be overwritten
+import lang.stride.*;
+
+/**
+ * 
+ */
+public class Persona
+{
+    private String nombre;
+
+    /**
+     * 
+     */
+    public Persona(String nombre)
+    {
+        this.nombre = nombre;
+    }
+
+    /**
+     * 
+     */
+    public void setPersona(String nombre)
+    {
+        this.nombre = nombre;
+    }
+
+    /**
+     * 
+     */
+    public String getPersona()
+    {
+        return nombre;
+    }
+}
