34b942bbe54bc45689805f84328fd9e46ef2c54b
Malderna <142558736+Malderna@users.noreply.github.com>
2023-09-05 16:41:20 -0400

    Add files via upload

---
+++ TE__Cabecerass/src/main/java/com/holamundo/te__cabecerass/Kevin_CSV.java
@@ -0,0 +1,29 @@
+package com.holamundo.te__cabecerass;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "RespCSV", urlPatterns = {"/RespCSV"})
+public class Kevin_CSV extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("application/json");
+       // response.setHeader("Content-Disposition","attachment;filename=archivo.xls");
+        PrintWriter out= response.getWriter();
+        try{
+        out.println("(\"empleados\":[");
+        out.println("(\"nombre completo\":\"Jose Flores Diaz\"");
+        out.println("(\"nombre completo\":\"Juan jose Mamani Paredes\"");
+        out.println("(\"nombre completo\":\"Dolores Fuertes Ortis");
+        out.println("])");   
+        }finally{
+        out.close();
+        }
+    }
+}
---
+++ TE__Cabecerass/src/main/java/com/holamundo/te__cabecerass/Kevin_XML.java
@@ -0,0 +1,44 @@
+package com.holamundo.te__cabecerass;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet(name = "RespXML", urlPatterns = {"/RespXML"})
+public class Kevin_XML extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        response.setContentType("application/XML");
+        PrintWriter out=response.getWriter();
+        try{
+            out.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+        out.println("<CATALOGO>");           
+        out.println("<CD>");           
+        out.println("<TITULO>Tinta</TITULO>");           
+        out.println("<ARTISTA>Andres Calamaro</ARTISTA>");           
+        out.println("<ORIGEN>BOLIVIA</ORIGEN>");  
+        out.println("<PRECIO>10 BS</PRECIO>");  
+        out.println("<AñO>2006</AñO>");  
+        out.println("</CD>");  
+        out.println("<CD>");  
+        out.println("<TITULO>La lengua popular Y ORIGINARIO</TITULO>");           
+        out.println("<ARTISTA>Andres Calamaro</ARTISTA>");           
+        out.println("<ORIGEN>BOLIVIA</ORIGEN>");    
+        out.println("</CD>");  
+        out.println("</CATALOGO>");           
+        }
+        finally{
+        out.close();
+        }
+        
+        
+    }
+
+  
+    
+
+}
